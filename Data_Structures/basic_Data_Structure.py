13677647e3dd146900d5c602d860583050cf7c47
aabine <kendoaustine@yahoo.com>
2024-08-29 17:52:19 +0100

    First commit

--- Data_Structures/basic_Data_Structure.py
+++ Data_Structures/basic_Data_Structure.py
@@ -9,6 +9,28 @@ begin_list[3] = 87
 #add and remove elements from the list
 begin_list.append(100)
 begin_list.remove(87)
+#insert element at a specific index
+begin_list.insert(3, 100)
+#remove element at a specific index
+begin_list.pop(2)
+#search the list for an element
+num = 0
+if num not in begin_list:
+    print("not found")
+else:
+    print("found")
+try:
+    print(begin_list.index(0))
+except ValueError:
+    print("0 is not in the list")
+#count the number of times an element appears in the list
+print(begin_list.count(5))
+#sort the list
+begin_list.sort()
+#reverse the list
+begin_list.reverse()
+
+
 
 
 
@@ -18,5 +40,6 @@ begin_list.remove(87)
 
 print(begin_list)
 
-
+#remove all elements from the list
+begin_list.clear()
 
